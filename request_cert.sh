7075963184e18da29d114f9f47d09f9397cd2cd1
Ubuntu <johnsonp@vmhk.d4o33zlcgyre3aanz15dk5jhgf.hx.internal.cloudapp.net>
2017-06-01 07:09:48 +0000

    Update request_cert.sh

--- request_cert.sh
+++ request_cert.sh
@@ -3,10 +3,12 @@
 DOMAIN=$1
 EMAIL=$2
 
-git clone https://github.com/certbot/certbot.git
-cd certbot
-./certbot-auto certonly -n --email $EMAIL --domains "$DOMAIN" --agree-tos --standalone --text
-cd ..
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install -y certbot
+sudo certbot certonly -n --email $EMAIL --domains "$DOMAIN" --agree-tos --standalone --text
+
 echo Copying server certificate to config directory...
 sudo cp -L /etc/letsencrypt/live/$DOMAIN/fullchain.pem config/serverCert.pem
 sudo cp -L /etc/letsencrypt/live/$DOMAIN/privkey.pem config/serverKey.pem
