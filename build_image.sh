2a89196210c895fd85a388235aaa3a8663a5fb5b
Johnson Pan <panjunsheng@msn.com>
2017-05-31 22:06:14 -0700

    Update build script

--- build_image.sh
+++ build_image.sh
@@ -9,3 +9,6 @@ echo Username: vpnuser
 echo Password: $PASSWORD
 sed -i "s/VPNUSER_PWD/$PASSWORD/g" config/ipsec.secrets
 docker build . -t vpn
+echo Successfully built docker image with name "vpn"
+echo Run: docker run -d -p 500:500/udp -p 4500:4500/udp --cap-add=NET_ADMIN -v $(pwd)/config:/home/config vpn
+echo to start the server
