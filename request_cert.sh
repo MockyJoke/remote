3e09e7384fed8a116d363ba338ffd640b2600df3
Ubuntu <johnsonp@v1.ojczu3jagh3ezpbwg50ytrndfg.dx.internal.cloudapp.net>
2017-06-01 04:36:05 +0000

    Add request_cert.sh

---
+++ request_cert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DOMAIN=$1
+EMAIL=$2
+
+git clone https://github.com/certbot/certbot.git
+cd certbot
+./certbot-auto certonly -n --email $EMAIL --domains "$DOMAIN" --agree-tos --standalone --text
+cd ..
+echo Copying server certificate to config directory...
+sudo cp -L /etc/letsencrypt/live/$DOMAIN/cert.pem config/serverCert.pem
+sudo cp -L /etc/letsencrypt/live/$DOMAIN/privkey.pem config/serverKey.pem
+echo Done!
