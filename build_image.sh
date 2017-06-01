42ed69946062400ca3a3bc283b0d05e588a8e108
Johnson Pan <panjunsheng@msn.com>
2017-05-31 22:01:53 -0700

    Randomly generate initial password

--- build_image.sh
+++ build_image.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 DOMAIN=$1
-
-sed -i 's/VPNSERVER_FQDN/$DOMAIN/g' ipsec.conf
-sed -i 's/VPNSERVER_FQDN/$DOMAIN/g' ipsec.secret
-dockek build . -t vpn
+PASSWORD=$(cat /dev/urandom | tr -dc '0-9' | fold -w 256 | head -n 1 | head --bytes 8)
+sed -i "s/VPNSERVER_FQDN/$DOMAIN/g" config/ipsec.conf
+sed -i "s/VPNSERVER_FQDN/$DOMAIN/g" config/ipsec.secrets
+echo Randomly generating default vpn username/PASSWORD
+echo Username: vpnuser
+echo Password: $PASSWORD
+sed -i "s/VPNUSER_PWD/$PASSWORD/g" config/ipsec.secrets
+docker build . -t vpn
