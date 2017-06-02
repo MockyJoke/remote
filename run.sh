81315531246c84c4f3b7a10258986fc6dc2eb92e
Johnson Pan <panjunsheng@msn.com>
2017-06-02 02:40:48 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ cp serverKey.pem /etc/ipsec.d/private
 cp serverCert.pem /etc/ipsec.d/certs
 cp caCert.pem /etc/ipsec.d/cacerts
 sysctl -w net.ipv4.ip_forward=1
-sysctl -w net.ipv4.ip_no_pmtu_disc=1
+#sysctl -w net.ipv4.ip_no_pmtu_disc=1
 iptables -A INPUT -p udp --dport  500 -j ACCEPT
 iptables -A INPUT -p udp --dport 4500 -j ACCEPT
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
