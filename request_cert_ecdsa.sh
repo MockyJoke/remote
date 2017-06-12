6fd8439b977dc57bebcd1c262e3d46c63eb555c6
Ubuntu <johnsonp@ezbox.ojczu3jagh3ezpbwg50ytrndfg.dx.internal.cloudapp.net>
2017-06-12 07:09:28 +0000

    Update ecdsa cert gen script

--- request_cert_ecdsa.sh
+++ request_cert_ecdsa.sh
@@ -8,9 +8,14 @@ sudo add-apt-repository -y ppa:certbot/certbot
 sudo apt-get update
 sudo apt-get install -y certbot
 
-openssl ecparam -genkey -name secp384r1 | openssl ec -out ecdsa/ec.key
+sudo rm ecdsa/*.pem
+sudo rm ecdsa/ec.*
+#openssl ecparam -genkey -name secp384r1 | openssl ec -out ecdsa/ec.key
+openssl ecparam -genkey -name secp384r1 -noout -out ecdsa/ec.key
 openssl req -new -sha384 -key ecdsa/ec.key -subj "/CN=$DOMAIN" -reqexts SAN -config <(cat ecdsa/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:$DOMAIN")) -outform der -out ecdsa/ec.csr
-sudo certbot certonly -n --email $EMAIL --domains "$DOMAIN" --agree-tos --standalone --text --csr ecdsa/ec.csr -w ecdsa
+cd ecdsa
+sudo certbot certonly -n --email $EMAIL --domains "$DOMAIN" --agree-tos --standalone --text --csr ec.csr
+cd ..
 
 echo Copying server certificate to config directory...
 sudo cp ecdsa/0001_chain.pem config/serverCert.pem
